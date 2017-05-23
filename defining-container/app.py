8d502919d24c0f3914954896bb067382954d27a6
Jeanderson Candido <jeandersonbc@gmail.com>
2017-05-23 10:07:48 -0300

    Fixed errors in app.py

--- defining-container/app.py
+++ defining-container/app.py
@@ -8,7 +8,7 @@ redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
-@app.rout("/")
+@app.route("/")
 def hello():
     try:
         visits = redis.incr("counter")
@@ -26,4 +26,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host:"0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80)
